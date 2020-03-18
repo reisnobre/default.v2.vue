b593cc12373e780377e73314c4082e8ba3ab85b5
Eduardo Reis Nobre <eduardo.reisnobre@gmail.com>
2020-03-18 11:05:48 -0300

    title on vue.config

--- vue.config.js
+++ vue.config.js
@@ -27,6 +27,10 @@ module.exports = {
     const svgRule = config.module.rule('svg')
     svgRule.uses.clear()
     svgRule.use('vue-svg-loader').loader('vue-svg-loader')
+    config.plugin('html').tap(args => {
+      args[0].title = 'Title goes Here'
+      return args
+    })
   },
   configureWebpack: () => {
     if (process.env.NODE_ENV !== 'production') return
