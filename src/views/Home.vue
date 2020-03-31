780750cf0784815dbd4a684d091b4d5d58d087e4
Eduardo Reis Nobre <eduardo.reisnobre@gmail.com>
2020-03-31 09:15:04 -0300

    jsconfig file

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,6 +12,50 @@
  * @displayName Home
  */
 export default {
-  name: 'home'
+  name: 'home',
+  methods: {
+    /**
+     * Gets called when the user clicks on the button
+     * @public
+     */
+    get () {
+    },
+    /**
+     * Gets called when the user clicks on the button
+     *
+     * @param {SyntheticEvent} event The react `SyntheticEvent`
+     * @param {Number} num Numbers of examples
+     * @public This is a public method
+     */
+    find (id) {
+    },
+    /**
+     * Gets called when the user clicks on the button
+     *
+     * @param {SyntheticEvent} event The react `SyntheticEvent`
+     * @param {Number} num Numbers of examples
+     * @public This is a public method
+     */
+    create (client) {
+    },
+    /**
+     * Gets called when the user clicks on the button
+     *
+     * @param {SyntheticEvent} event The react `SyntheticEvent`
+     * @param {Number} num Numbers of examples
+     * @public This is a public method
+     */
+    edit (client) {
+    },
+    /**
+     * Gets called when the user clicks on the button
+     *
+     * @param {SyntheticEvent} event The react `SyntheticEvent`
+     * @param {Number} num Numbers of examples
+     * @public This is a public method
+     */
+    delete (id) {
+    }
+  }
 }
 </script>
